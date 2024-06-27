8183ef96cb8df536d5baa6a26575ffe199e55da3
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2024-06-27 08:52:50 -0600

    Adds descriptions to filters.

--- Application/EdFi.Ods.AdminApi/Infrastructure/Documentation/SwaggerDefaultParameterFilter.cs
+++ Application/EdFi.Ods.AdminApi/Infrastructure/Documentation/SwaggerDefaultParameterFilter.cs
@@ -29,11 +29,136 @@ public class SwaggerDefaultParameterFilter : IOperationFilter
                 parameter.Description = "Indicates how many items should be skipped before returning results.";
                 parameter.Schema.Default = new OpenApiString(_settings.Value.DefaultPageSizeOffset.ToString());
             }
-            if (parameter.Name.ToLower().Equals("limit"))
+            else if (parameter.Name.ToLower().Equals("limit"))
             {
                 parameter.Description = "Indicates the maximum number of items that should be returned in the results.";
                 parameter.Schema.Default = new OpenApiString(_settings.Value.DefaultPageSizeLimit.ToString());
             }
+        }
+
+        switch (context.MethodInfo.Name)
+        {
+            case "GetProfiles":
+                {
+                    foreach (var parameter in operation.Parameters)
+                    {
+                        if (parameter.Name.ToLower().Equals("id"))
+                        {
+                            parameter.Description = "Use this field to filter by Profile Id";
+                        }
+                        else if (parameter.Name.ToLower().Equals("name"))
+                        {
+                            parameter.Description = "Use this field to filter by Profile Name";
+                        }
+                    }
+                    break;
+                }
+            case "GetResourceClaims":
+                {
+                    foreach (var parameter in operation.Parameters)
+                    {
+                        if (parameter.Name.ToLower().Equals("id"))
+                        {
+                            parameter.Description = "Use this field to filter by Resource Claim Id";
+                        }
+                        else if (parameter.Name.ToLower().Equals("name"))
+                        {
+                            parameter.Description = "Use this field to filter by Resource Claim Name";
+                        }
+                    }
+                    break;
+                }
+            case "GetVendors":
+                {
+                    foreach (var parameter in operation.Parameters)
+                    {
+                        if (parameter.Name.ToLower().Equals("id"))
+                        {
+                            parameter.Description = "Use this field to filter by Vendor Id";
+                        }
+                        else if (parameter.Name.ToLower().Equals("company"))
+                        {
+                            parameter.Description = "Use this field to filter by Company Name";
+                        }
+                        else if (parameter.Name.ToLower().Equals("namespaceprefixes"))
+                        {
+                            parameter.Description = "Use this field to filter by Namespace Prefix";
+                        }
+                        else if (parameter.Name.ToLower().Equals("contactname"))
+                        {
+                            parameter.Description = "Use this field to filter by Contact Name";
+                        }
+                        else if (parameter.Name.ToLower().Equals("contactemailaddress"))
+                        {
+                            parameter.Description = "Use this field to filter by Contact Email Address";
+                        }
+                    }
+                    break;
+                }
+            case "GetOdsInstances":
+                {
+                    foreach (var parameter in operation.Parameters)
+                    {
+                        if (parameter.Name.ToLower().Equals("id"))
+                        {
+                            parameter.Description = "Use this field to filter by Ods Instance Id";
+                        }
+                        else if (parameter.Name.ToLower().Equals("name"))
+                        {
+                            parameter.Description = "Use this field to filter by Ods Instance Name";
+                        }
+                    }
+                    break;
+                }
+            case "GetClaimSets":
+                {
+                    foreach (var parameter in operation.Parameters)
+                    {
+                        if (parameter.Name.ToLower().Equals("id"))
+                        {
+                            parameter.Description = "Use this field to filter by Claimset Id";
+                        }
+                        else if (parameter.Name.ToLower().Equals("name"))
+                        {
+                            parameter.Description = "Use this field to filter by Claimset Name";
+                        }
+                    }
+                    break;
+                }
+            case "GetApplications":
+                {
+                    foreach (var parameter in operation.Parameters)
+                    {
+                        if (parameter.Name.ToLower().Equals("id"))
+                        {
+                            parameter.Description = "Use this field to filter by Profile Id";
+                        }
+                        else if (parameter.Name.ToLower().Equals("applicationname"))
+                        {
+                            parameter.Description = "Use this field to filter by Application Name";
+                        }
+                        else if (parameter.Name.ToLower().Equals("claimsetname"))
+                        {
+                            parameter.Description = "Use this field to filter by Claimset Name";
+                        }
+                    }
+                    break;
+                }
+            case "GetActions":
+                {
+                    foreach (var parameter in operation.Parameters)
+                    {
+                        if (parameter.Name.ToLower().Equals("id"))
+                        {
+                            parameter.Description = "Use this field to filter by Actions Id";
+                        }
+                        else if (parameter.Name.ToLower().Equals("name"))
+                        {
+                            parameter.Description = "Use this field to filter by Actions Name";
+                        }
+                    }
+                    break;
+                }
         }
     }
 }
