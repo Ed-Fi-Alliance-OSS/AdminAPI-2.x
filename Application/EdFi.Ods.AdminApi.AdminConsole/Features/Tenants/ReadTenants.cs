7ef65636323e89f5beff52de2e2e55901a4d67a1
Ricardo Gamboa <rgamboaa@wearegap.com>
2024-11-05 11:40:51 -0600

    get tenant changes

--- Application/EdFi.Ods.AdminApi.AdminConsole/Features/Tenants/ReadTenants.cs
+++ Application/EdFi.Ods.AdminApi.AdminConsole/Features/Tenants/ReadTenants.cs
@@ -15,10 +15,10 @@ public class ReadTenants : IFeature
 {
     public void MapEndpoints(IEndpointRouteBuilder endpoints)
     {
-        AdminApiAdminConsoleEndpointBuilder.MapGet(endpoints, "/tenants", GetTenants)
+        AdminApiAdminConsoleEndpointBuilder.MapGet(endpoints, "/tenant", GetTenants)
            .BuildForVersions();
 
-        AdminApiAdminConsoleEndpointBuilder.MapGet(endpoints, "/tenant", GetTenant)
+        AdminApiAdminConsoleEndpointBuilder.MapGet(endpoints, "/tenant/{tenantId}", GetTenant)
            .BuildForVersions();
     }
 
