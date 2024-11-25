dbf3f51349f2bfa6e57b578806a4b0d756cf1583
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2024-11-25 14:30:24 -0600

    Rebase from main branch

--- Application/EdFi.Ods.AdminApi.Common/Infrastructure/Helpers/CorsSettings.cs
+++ Application/EdFi.Ods.AdminApi.Common/Infrastructure/Helpers/CorsSettings.cs
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:Application/EdFi.Ods.AdminApi.AdminConsole/Helpers/CorsSettings.cs
 // SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
@@ -17,23 +16,3 @@ public class CorsSettings
     public bool EnableCors { get; set; }
     public string[] AllowedOrigins { get; set; } = new string[0];
 }
-========
-// SPDX-License-Identifier: Apache-2.0
-// Licensed to the Ed-Fi Alliance under one or more agreements.
-// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
-// See the LICENSE and NOTICES files in the project root for more information.
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EdFi.Ods.AdminApi.Common.Helpers;
-
-public class CorsSettings
-{
-    public bool EnableCors { get; set; }
-    public string[]? AllowedOrigins { get; set; }
-}
->>>>>>>> a7de2d9b (Fix CORS settings):Application/EdFi.Ods.AdminApi.Common/Infrastructure/Helpers/CorsSettings.cs
