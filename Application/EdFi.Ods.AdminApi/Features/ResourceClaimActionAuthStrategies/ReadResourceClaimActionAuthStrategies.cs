041e3356d426d92d9cb73933a7941192d018c071
Danny Fernández Alvarado <dfernandeza@growthaccelerationpartners.com>
2025-01-27 11:30:10 -0600

    Fix using statements

--- Application/EdFi.Ods.AdminApi/Features/ResourceClaimActionAuthStrategies/ReadResourceClaimActionAuthStrategies.cs
+++ Application/EdFi.Ods.AdminApi/Features/ResourceClaimActionAuthStrategies/ReadResourceClaimActionAuthStrategies.cs
@@ -4,8 +4,7 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using AutoMapper;
-using EdFi.Ods.AdminApi.Common.Features;
-using EdFi.Ods.AdminApi.Common.Infrastructure;
+using EdFi.Ods.AdminApi.Infrastructure;
 using EdFi.Ods.AdminApi.Infrastructure.Database.Queries;
 
 namespace EdFi.Ods.AdminApi.Features.ResourceClaimActionAuthStrategies;
