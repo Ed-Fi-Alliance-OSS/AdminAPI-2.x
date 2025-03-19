de8ea9ee0354f25ecdbec03d77689ac8c03e55ba
Juan Agudelo <jagudelo@wearegap.com>
2025-03-19 15:22:56 -0500

    Fix status error code to return when conflict

--- Application/EdFi.Ods.AdminApi.AdminConsole/Features/Instances/DeleteInstance.cs
+++ Application/EdFi.Ods.AdminApi.AdminConsole/Features/Instances/DeleteInstance.cs
@@ -3,16 +3,13 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.DataAccess.Models;
-using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Repositories;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services.Instances.Commands;
-using EdFi.Ods.AdminApi.Common.Constants;
 using EdFi.Ods.AdminApi.Common.Features;
 using EdFi.Ods.AdminApi.Common.Infrastructure;
+using EdFi.Ods.AdminApi.Common.Infrastructure.ErrorHandling;
 using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
 
 namespace EdFi.Ods.AdminApi.AdminConsole.Features.Instances;
 public class DeleteInstance : IFeature
@@ -24,44 +21,21 @@ public class DeleteInstance : IFeature
         .BuildForVersions(AdminApiVersions.AdminConsole);
     }
 
-    private static async Task<IResult> Execute(int id, DeleteInstanceValidator validator, IPendingDeleteInstanceCommand changeStatusInstanceCommand)
+    private static async Task<IResult> Execute(int id, IPendingDeleteInstanceCommand changeStatusInstanceCommand)
     {
-        await validator.ValidateAsync(id);
-        await changeStatusInstanceCommand.Execute(id);
+        try
+        {
+            await changeStatusInstanceCommand.Execute(id);
+        }
+        catch (NotFoundException<int> ex)
+        {
+            return Results.NotFound(ex.Message);
+        }
+        catch (ValidationException ex)
+        {
+            return Results.Conflict(ex.Errors.Count() == 1 ? ex.Errors.First().ErrorMessage : ex.Message);
+        }
+
         return Results.StatusCode(StatusCodes.Status202Accepted);
     }
 }
-
-public class DeleteInstanceValidator : AbstractValidator<int>
-{
-    private readonly IQueriesRepository<Instance> _instanceQuery;
-    public DeleteInstanceValidator(IQueriesRepository<Instance> instanceQuery)
-    {
-        _instanceQuery = instanceQuery;
-
-        RuleFor(x => x)
-            .GreaterThan(0)
-            .WithMessage(AdminConsoleValidationConstants.OdsIntanceIdIsNotValid);
-
-        RuleFor(x => x)
-            .MustAsync(IsStatusCompleted)
-            .WithMessage(AdminConsoleValidationConstants.OdsIntanceIdStatusIsNotCompleted)
-            .WhenAsync(Exist);
-
-    }
-
-    private async Task<bool> Exist(int id, CancellationToken cancellationToken)
-    {
-        var existingInstance = await _instanceQuery.Query()
-            .FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
-        return existingInstance != null;
-    }
-
-    private async Task<bool> IsStatusCompleted(int id, CancellationToken cancellationToken)
-    {
-        var existingInstance = await _instanceQuery.Query()
-            .FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
-        return existingInstance!.Status == InstanceStatus.Completed;
-    }
-}
-
