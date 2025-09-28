8d47e0b630051fd3ed4f41e9cf0553420bdb70c4
Stephen A. Fuqua <stephen@safnet.com>
2025-09-28 16:15:06 -0500

    Reformat file

--- Application/EdFi.Ods.AdminApi/Features/RequestLoggingMiddleware.cs
+++ Application/EdFi.Ods.AdminApi/Features/RequestLoggingMiddleware.cs
@@ -3,12 +3,12 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Net;
+using System.Text.Json;
 using EdFi.Common.Utils.Extensions;
 using EdFi.Ods.AdminApi.Common.Infrastructure.ErrorHandling;
 using FluentValidation;
 using log4net;
-using System.Net;
-using System.Text.Json;
 
 namespace EdFi.Ods.AdminApi.Features;
 
@@ -29,11 +29,19 @@ public class RequestLoggingMiddleware
             if (context.Request.Path.StartsWithSegments(new PathString("/.well-known")))
             {
                 // Requests to the OpenId Connect ".well-known" endpoint are too chatty for informational logging, but could be useful in debug logging.
-                _logger.Debug(JsonSerializer.Serialize(new { path = context.Request.Path.Value, traceId = context.TraceIdentifier }));
+                _logger.Debug(
+                    JsonSerializer.Serialize(
+                        new { path = context.Request.Path.Value, traceId = context.TraceIdentifier }
+                    )
+                );
             }
             else
             {
-                _logger.Info(JsonSerializer.Serialize(new { path = context.Request.Path.Value, traceId = context.TraceIdentifier }));
+                _logger.Info(
+                    JsonSerializer.Serialize(
+                        new { path = context.Request.Path.Value, traceId = context.TraceIdentifier }
+                    )
+                );
             }
 
             // Check if this is a token endpoint request and intercept the response
@@ -91,7 +99,17 @@ public class RequestLoggingMiddleware
             // Check if response has already started or stream is closed
             if (response.HasStarted)
             {
-                _logger.Error(JsonSerializer.Serialize(new { message = "Cannot write to response, response has already started", error = new { ex.Message, ex.StackTrace }, traceId = context.TraceIdentifier }), ex);
+                _logger.Error(
+                    JsonSerializer.Serialize(
+                        new
+                        {
+                            message = "Cannot write to response, response has already started",
+                            error = new { ex.Message, ex.StackTrace },
+                            traceId = context.TraceIdentifier
+                        }
+                    ),
+                    ex
+                );
                 return;
             }
 
@@ -116,7 +134,11 @@ public class RequestLoggingMiddleware
                     });
 
 #pragma warning disable S6667 // Logging in a catch clause should pass the caught exception as a parameter.
-                    _logger.Debug(JsonSerializer.Serialize(new { message = validationResponse, traceId = context.TraceIdentifier }));
+                    _logger.Debug(
+                        JsonSerializer.Serialize(
+                            new { message = validationResponse, traceId = context.TraceIdentifier }
+                        )
+                    );
 #pragma warning restore S6667 // Logging in a catch clause should pass the caught exception as a parameter.
 
                     response.StatusCode = (int)HttpStatusCode.BadRequest;
@@ -124,29 +146,61 @@ public class RequestLoggingMiddleware
                     break;
 
                 case INotFoundException notFoundException:
-                    var notFoundResponse = new
-                    {
-                        title = notFoundException.Message,
-                    };
-                    _logger.Debug(JsonSerializer.Serialize(new { message = notFoundResponse, traceId = context.TraceIdentifier }));
+                    var notFoundResponse = new { title = notFoundException.Message, };
+                    _logger.Debug(
+                        JsonSerializer.Serialize(
+                            new { message = notFoundResponse, traceId = context.TraceIdentifier }
+                        )
+                    );
 
                     response.StatusCode = (int)HttpStatusCode.NotFound;
                     await response.WriteAsync(JsonSerializer.Serialize(notFoundResponse));
                     break;
 
                 case IAdminApiException adminApiException:
-                    var message = adminApiException.StatusCode.HasValue && !string.IsNullOrWhiteSpace(adminApiException.Message)
-                        ? adminApiException.Message
-                        : "The server encountered an unexpected condition that prevented it from fulfilling the request.";
-                    _logger.Error(JsonSerializer.Serialize(new { message = "An uncaught error has occurred", error = new { ex.Message, ex.StackTrace }, traceId = context.TraceIdentifier }), ex);
-                    response.StatusCode = adminApiException.StatusCode.HasValue ? (int)adminApiException.StatusCode : 500;
+                    var message =
+                        adminApiException.StatusCode.HasValue
+                        && !string.IsNullOrWhiteSpace(adminApiException.Message)
+                            ? adminApiException.Message
+                            : "The server encountered an unexpected condition that prevented it from fulfilling the request.";
+                    _logger.Error(
+                        JsonSerializer.Serialize(
+                            new
+                            {
+                                message = "An uncaught error has occurred",
+                                error = new { ex.Message, ex.StackTrace },
+                                traceId = context.TraceIdentifier
+                            }
+                        ),
+                        ex
+                    );
+                    response.StatusCode = adminApiException.StatusCode.HasValue
+                        ? (int)adminApiException.StatusCode
+                        : 500;
                     await response.WriteAsync(JsonSerializer.Serialize(new { message = message }));
                     break;
 
                 default:
-                    _logger.Error(JsonSerializer.Serialize(new { message = "An uncaught error has occurred", error = new { ex.Message, ex.StackTrace }, traceId = context.TraceIdentifier }), ex);
+                    _logger.Error(
+                        JsonSerializer.Serialize(
+                            new
+                            {
+                                message = "An uncaught error has occurred",
+                                error = new { ex.Message, ex.StackTrace },
+                                traceId = context.TraceIdentifier
+                            }
+                        ),
+                        ex
+                    );
                     response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    await response.WriteAsync(JsonSerializer.Serialize(new { message = "The server encountered an unexpected condition that prevented it from fulfilling the request." }));
+                    await response.WriteAsync(
+                        JsonSerializer.Serialize(
+                            new
+                            {
+                                message = "The server encountered an unexpected condition that prevented it from fulfilling the request."
+                            }
+                        )
+                    );
                     break;
             }
         }
