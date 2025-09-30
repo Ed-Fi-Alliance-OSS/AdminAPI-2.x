7b7102f33232f3a10698b43af10f7a25fa47b888
David Jimenez <djimenez@growthaccelerationpartners.com>
2025-09-30 14:33:01 -0600

    Implements Tenant Endpoints on V2

--- Application/EdFi.Ods.AdminApi/Features/RequestLoggingMiddleware.cs
+++ Application/EdFi.Ods.AdminApi/Features/RequestLoggingMiddleware.cs
@@ -94,6 +94,9 @@ public class RequestLoggingMiddleware
         }
         catch (Exception ex)
         {
+            Console.WriteLine("Error");
+            Console.WriteLine(ex);
+
             var response = context.Response;
 
             // Check if response has already started or stream is closed
