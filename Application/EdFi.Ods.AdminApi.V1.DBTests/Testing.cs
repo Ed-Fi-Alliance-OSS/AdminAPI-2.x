f00e935cca18a662e329610620d81ec076e0dbe7
David Jimenez <djimenez@growthaccelerationpartners.com>
2025-10-15 11:04:48 -0600

    Copy over the integration tests from V1

---
+++ Application/EdFi.Ods.AdminApi.V1.DBTests/Testing.cs
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Ods.AdminApi.Common.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace EdFi.Ods.AdminApi.V1.DBTests;
+
+public static class Testing
+{
+    public static void EnsureInitialized()
+    {
+        _ = new SecurityTestDatabaseSetup();
+        SecurityTestDatabaseSetup.EnsureSecurityDatabase(@"C:\\temp");
+    }
+
+    private static IConfigurationRoot _config;
+
+    public static IConfiguration Configuration()
+    {
+        _config ??= new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+        return _config;
+    }
+
+    public static string AdminConnectionString { get { return Configuration().GetConnectionString("Admin"); } }
+
+    public static string SecurityConnectionString { get { return Configuration().GetConnectionString("Security"); } }
+
+    public static string SecurityV53ConnectionString { get { return Configuration().GetConnectionString("SecurityV53"); } }
+
+    public static int DefaultPageSizeOffset => (int)Configuration().GetValue(typeof(int), "AppSettings:DefaultPageSizeOffset");
+
+    public static int DefaultPageSizeLimit => (int)Configuration().GetValue(typeof(int), "AppSettings:DefaultPageSizeLimit");
+
+    public static DbContextOptions GetDbContextOptions(string connectionString)
+    {
+        var builder = new DbContextOptionsBuilder();
+        builder.UseSqlServer(connectionString);
+        return builder.Options;
+    }
+
+    public static IOptions<AppSettings> GetAppSettings()
+    {
+        AppSettings appSettings = new()
+        {
+            DefaultPageSizeOffset = DefaultPageSizeOffset,
+            DefaultPageSizeLimit = DefaultPageSizeLimit
+        };
+        IOptions<AppSettings> options = Options.Create(appSettings);
+        return options;
+    }
+}
